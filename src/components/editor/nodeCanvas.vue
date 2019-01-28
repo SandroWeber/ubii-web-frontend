a3544afb6a73a468846b6274b8bb12582e10a98d
daniel <dyrda@in.tum.de>
2019-01-28 14:59:54 +0100

    Added nodeCanvas component

---
+++ src/components/editor/nodeCanvas.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="node-canvas">
+      <div class="blueprint-grid">
+        <svg width="100%" height="100%">
+            <defs>
+                <pattern id="dots" x="0" y="0" width="25" height="25" patternUnits="userSpaceOnUse">
+                    <circle class="blueprint-dot" cx="10" cy="10" r="2" />
+                </pattern>
+            </defs>
+
+            <rect x="0" y="0" width="100%" height="100%" fill="url(#dots)" />
+        </svg>
+    </div>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'nodeCanvas',
+    components: {}
+  }
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+  .node-canvas {
+    width: 100%;
+    height: 100%;
+  }
+  .blueprint-grid {
+    width: 100%;
+    height: 100%;
+    background-color: #eeeeee;
+  }
+  .blueprint-dot {
+    fill: #cccccc
+  }
+</style>
