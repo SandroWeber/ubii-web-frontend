f9dedd22068d861316858266da8b920dd48e2cff
Daniel <dyrda@in.tum.de>
2019-02-06 14:14:34 +0100

    Updated app

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,13 @@
 <template>
-  <div id="app">
-    <server-status />
-    <nav class="navigation-bar">
-      <router-link to="/" class="navigation-item">Home</router-link> |
+  <div id="app" class="layer-one background">
+    <server-status id="server-status"/>
+    <nav class="navigation-bar layer-one background border">
+      <router-link to="/" class="navigation-item ">Home</router-link> |
       <router-link to="/admin" class="navigation-item">Administration</router-link> |
       <router-link to="/tools" class="navigation-item">Tools</router-link> |
       <router-link to="/nodeEditor" class="navigation-item">Node Editor</router-link>
     </nav>
+    
     <router-view class="router-view"/>
   </div>
 </template>
@@ -28,42 +29,42 @@
   }
 </script>
 
-<style>
+<style lang="stylus"> 
   * {
       margin: 0;
       padding: 0;
   }
-  
-  html, body {
-    margin: 0;
-    width: 100%;
-    height: 100%;
-  }
 
+  html, body {
+    width: 100%;   
+    height: 100%; 
+  } 
+ 
   #app {
     font-family: 'Avenir', Helvetica, Arial, sans-serif;
     -webkit-font-smoothing: antialiased;
     -moz-osx-font-smoothing: grayscale;
-    background: white;
     width: 100%;
     height: 100%;
   }
 
   .navigation-bar {
-    padding: 10px;
+    padding: 15px;
     text-align: center;
-    background: dimgrey;
+    z-index: 900
   }
 
   .navigation-item {
-    color: cyan;
     padding: 0px 20px 0px 20px;
+    text-decoration: none
   }
+
+  #server-status
+    position: relative
+    z-index: 1000
 
   .server-stats {
     text-align: center;
   }
 
-  .router-view {
-  }
 </style>
