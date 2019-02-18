4f9e6906c1d44be9bebe558e26f0a1f3480fd246
Daniel <dyrda@in.tum.de>
2019-02-18 09:30:41 +0100

    Updated the interaction item

--- src/components/editor/interactionEditor/interactionItem.vue
+++ src/components/editor/interactionEditor/interactionItem.vue
@@ -1,16 +1,22 @@
 <template>
-  <div class="interaction-item low-contrast" @click="selectItem(id)">
-        <span>{{name}}</span>
+  <div class="interaction-item low-contrast" @click="selectItem(id)" v-bind:class="{ selected: selected }">
+    <font-awesome-icon icon="code" class="icon"/>
+    <span class="label">{{name}}</span>
   </div>
 </template>
 
 <script>
+  import { library } from '@fortawesome/fontawesome-svg-core'
+  import { faCode } from '@fortawesome/free-solid-svg-icons'
+  library.add(faCode);
+
   export default { 
     name: 'interactionInterfaceToken',
     components: {},
     props: {
         id: String,
         name: String,
+        selected: Boolean
     },
     methods: {
         selectItem: function(id) {
@@ -22,7 +28,24 @@
 
 <style scoped lang="stylus"> 
   .interaction-item
+    height 20px
     order 1
     margin 5px
-    padding 5px 10px 5px 10px
+    padding 0px 10px 0px 10px
+    display flex
+    flex-direction row
+    cursor pointer
+    &:hover
+      color #ffffff
+    &.selected
+      background-color layerThreeSecondaryColor
+      color yellowAccentColor
+
+  .icon
+    height 16px
+    width 16px
+    margin-top: 1px
+    
+  .label
+    margin-left 5px
 </style>
