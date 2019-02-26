dbaa521c4c4ae41d663136607a820bc369bab826
daniel <dyrda@in.tum.de>
2019-02-26 13:22:34 +0100

    Updated EntryPage

--- src/components/EntryPage.vue
+++ src/components/EntryPage.vue
@@ -1,12 +1,17 @@
 <template>
   <div class="canvas layer-two background horizontal-shadow">
     <div class="grid">
-      <router-link to="/admin" class="interface-link layer-three background round shadow link-area green-accent">
-        <font-awesome-icon icon="cogs" class="interface-icon" />
+      <router-link  to="/admin"
+                    class="interface-link layer-three background round shadow link-area green-accent">
+        <font-awesome-icon  icon="cogs"
+                            class="interface-icon" />
         <p>Administration</p>
       </router-link>
-      <router-link to="/tools" class="interface-link layer-three background round shadow link-area blue-accent">
-        <font-awesome-icon icon="toolbox" class="interface-icon " />
+
+      <router-link  to="/tools"
+                    class="interface-link layer-three background round shadow link-area blue-accent">
+        <font-awesome-icon  icon="toolbox"
+                            class="interface-icon " />
         <p>Tools</p>
       </router-link>
     </div>
@@ -26,26 +31,22 @@
   }
 </script>
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped lang="stylus">
   .canvas
     height: 100% 
-    padding: 25px;
+    padding: 25px
   
-  .grid {
-    display: grid;
-    grid-gap: 15px;
-    grid-template-columns: 1fr 1fr 1fr 1fr;
-    
-  }
+  .grid
+    display: grid
+    grid-gap: 15px
+    grid-template-columns: 1fr 1fr 1fr 1fr
 
   .interface-link
-    padding: 10px;
-    text-align: center;
+    padding: 10px
+    text-align: center
 
 
-  .interface-icon {
-    width: 50px;
-    height: 50px;
-  }
+  .interface-icon
+    width: 50px
+    height: 50px
 </style>
