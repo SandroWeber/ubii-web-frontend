4327fda1d7cdc5e26c3275a11d631b5838b05d3b
Daniel <dyrda@in.tum.de>
2019-03-06 09:53:28 +0100

    interactions store module progress

--- src/store/modules/interactions.js
+++ src/store/modules/interactions.js
@@ -1,6 +1,7 @@
 // initial state
 const state = {
-  }
+  interactions: []
+}
   
 // getters
 const getters = {
@@ -8,10 +9,18 @@ const getters = {
 
 // actions
 const actions = {
+  addInteraction (context, payload) {
+    context.commit('increment', {
+      interaction: payload.interaction
+    })
+  }
 }
 
 // mutations
 const mutations = {
+  pushInteraction (state, payload){
+    state.interactions.push(payload.interaction);
+  }
 }
 
 export default {
