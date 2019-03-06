e15ddd69815e7574788605421a3a62c7198d88f6
Daniel <dyrda@in.tum.de>
2019-03-06 10:51:40 +0100

    Updated interactions store module

--- src/store/modules/interactions.js
+++ src/store/modules/interactions.js
@@ -1,10 +1,65 @@
+// Dummy interaction.
+let dummyInteractionOne =
+  {
+    id: '1234',
+    name: 'Dummy Interaction One',
+    processingCallback: `(input, output, state) => {
+  return true;
+}`,
+  inputFormats: [
+    {
+        internalName: 'input-A',
+        messageFormat: 'topic-A'
+    },
+    {
+        internalName: 'input-B',
+        messageFormat: 'topic-B'
+    }
+  ],
+  outputFormats: [
+    {
+        internalName: 'output-X',
+        messageFormat: 'topic-X'
+    },
+    {
+        internalName: 'output-Y',
+        messageFormat: 'topic-Y'
+    }
+  ]
+};
+
+let dummyInteractionTwo = {
+  id: 'uuidv4()',
+  name: 'mirror-mouse-pointer',
+  processingCallback: `(input, output, state) => {
+  return true;
+}`,
+  inputFormats: [
+    {
+      internalName: 'inputClientPointer.internalName',
+      messageFormat: 'inputClientPointer.messageFormat'
+    },
+    {
+      internalName: 'inputMirror.internalName',
+      messageFormat: 'inputMirror.messageFormat'
+    }
+  ],
+  outputFormats: [
+    {
+      internalName: 'outputServerPointer.internalName',
+      messageFormat: 'outputServerPointer.messageFormat'
+    }
+  ]
+};
+
 // initial state
 const state = {
-  interactions: []
+  interactions: [dummyInteractionOne, dummyInteractionTwo]
 }
   
 // getters
 const getters = {
+    flavor: state => state.flavor
 }
 
 // actions
