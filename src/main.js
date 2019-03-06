ec777193d5267508a15f2ee2d606c2626dc725df
Daniel <dyrda@in.tum.de>
2019-03-06 10:50:38 +0100

    Updated vue instance options

--- src/main.js
+++ src/main.js
@@ -1,7 +1,8 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
-import store from './store'
+import store from './store/index.js'
+
 
 /* font-awesome */
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
@@ -11,7 +12,7 @@ Vue.component('font-awesome-icon', FontAwesomeIcon);
 Vue.config.productionTip = false;
 
 new Vue({
-  router,
-  store,
-  render: h => h(App)
-}).$mount('#app');
+    store,
+    router,
+    render: h => h(App)
+  }).$mount('#app');
