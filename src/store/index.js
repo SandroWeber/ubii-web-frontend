d52195921b53665b8631651c04911e94fe6e73d0
Daniel <dyrda@in.tum.de>
2019-03-11 12:21:19 +0100

    Added sessions module to the store

--- src/store/index.js
+++ src/store/index.js
@@ -1,13 +1,13 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
 import interactions from './modules/interactions'
-//import sessions from './modules/sessions'
+import sessions from './modules/sessions'
 
 Vue.use(Vuex)
 
 export default new Vuex.Store({
   modules: {
     interactions,
-   // sessions
+    sessions
   },
 })
