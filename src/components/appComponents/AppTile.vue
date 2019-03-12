df7b5c86bc4fbc1c3b5642c167674e5b1578f658
Daniel <dyrda@in.tum.de>
2019-03-12 11:01:13 +0100

    Implemented appTile component

--- src/components/appComponents/AppTile.vue
+++ src/components/appComponents/AppTile.vue
@@ -8,15 +8,52 @@
   export default { 
     name: 'AppTile',
     components: {},
-    props: {
-      name: String,
-    }
+    props: {}
   } 
 </script> 
 
 <style scoped lang="stylus"> 
-  .interaction-mirror-interface-list-token
-    order 1
-    margin 5px
-    padding 5px 10px 5px 10px
+  .app-tile
+    border-width: 2px
+    border-style: solid
+    text-decoration: none
+    color: mediumContrastColor
+    padding: 10px
+    
+    // Priamry
+    &.layer-one
+        border-color: layerOneBorderColor
+    &.layer-two
+        border-color: layerTwoBorderColor
+    &.layer-three
+        border-color: layerThreeBorderColor
+    &.layer-four
+        border-color: layerFourBorderColor
+
+    // Contrast
+    &.low-contrast
+        color: lowContrastColor
+    &.medium-contrast
+        color: mediumContrastColor
+    &.high-contrast
+        color: highContrastColor
+    &.max-contrast
+        color: maxContrastColor
+
+    // Accents:
+    &.red-accent
+        color: redAccentColor
+    &.green-accent
+        color: greenAccentColor
+    &.yellow-accent
+        color: yellowAccentColor
+    &.orange-accent
+        color: orangeAccentColor
+    &.purple-accent
+        color: purpleAccentColor
+    &.blue-accent
+        color: blueAccentColor
+    
+    &:hover
+      color: maxContrastColor
 </style>
