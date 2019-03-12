8cacd87c619494710f880639a15c70c2f4474a02
Daniel <dyrda@in.tum.de>
2019-03-12 10:50:09 +0100

    Added appTile component

---
+++ src/components/appComponents/appTile.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="app-tile">
+    <slot> </slot>
+  </div>
+</template>
+
+<script>
+  export default { 
+    name: 'AppTile',
+    components: {},
+    props: {
+      name: String,
+    }
+  } 
+</script> 
+
+<style scoped lang="stylus"> 
+  .interaction-mirror-interface-list-token
+    order 1
+    margin 5px
+    padding 5px 10px 5px 10px
+</style>
