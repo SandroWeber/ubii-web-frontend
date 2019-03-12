cbfbcd6e0e898cab97ae9afc095ddc086bdd400c
Daniel <dyrda@in.tum.de>
2019-03-12 11:12:05 +0100

    Added appIcon implementation base

---
+++ src/components/appComponents/AppIcon.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="app-icon">
+    <slot> </slot>
+  </div>
+</template>
+
+<script>
+  export default { 
+    name: 'AppIcon',
+    components: {},
+    props: {}
+  } 
+</script> 
+
+<style scoped lang="stylus"> 
+</style>
