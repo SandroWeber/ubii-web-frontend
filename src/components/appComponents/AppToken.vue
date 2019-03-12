9b2cb08341e96e33bff2789279af488a337bfeb5
Daniel <dyrda@in.tum.de>
2019-03-12 11:12:20 +0100

    Added appToken implementation base

---
+++ src/components/appComponents/AppToken.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="app-token">
+    <slot> </slot>
+  </div>
+</template>
+
+<script>
+  export default { 
+    name: 'AppToken',
+    components: {},
+    props: {}
+  } 
+</script> 
+
+<style scoped lang="stylus"> 
+
+</style>
