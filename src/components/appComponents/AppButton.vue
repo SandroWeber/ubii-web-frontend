a4e6b1787da729d083e053f53e505105cc15141c
Daniel <dyrda@in.tum.de>
2019-03-12 11:11:44 +0100

    Added appButton implementation base

---
+++ src/components/appComponents/AppButton.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="app-button">
+    <slot> </slot>
+  </div>
+</template>
+
+<script>
+  export default { 
+    name: 'AppButton',
+    components: {},
+    props: {}
+  } 
+</script> 
+
+<style scoped lang="stylus"> 
+</style>
