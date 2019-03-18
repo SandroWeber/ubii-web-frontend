21e6c5a7f2442a2ae7f1fb86b5f378c87d7cec1a
daniel <dyrda@in.tum.de>
2019-03-18 10:48:10 +0100

    Added AppLayer component

---
+++ src/components/appComponents/AppLayer.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="app-layer">
+    <slot> </slot>
+  </div>
+</template>
+
+<script>
+  export default { 
+    name: 'AppLayer',
+    components: {},
+    props: {}
+  } 
+</script> 
+
+<style scoped lang="stylus"> 
+</style>
