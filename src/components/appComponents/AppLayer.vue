6b556dd871f353c4d284c6c8989cb0fdb5e8cc98
daniel <dyrda@in.tum.de>
2019-03-18 12:43:39 +0100

    Updated AppLayer component

--- src/components/appComponents/AppLayer.vue
+++ src/components/appComponents/AppLayer.vue
@@ -12,5 +12,58 @@
   } 
 </script> 
 
-<style scoped lang="stylus"> 
+<style scoped lang="stylus">
+@import "./../../styles/main/base/color"
+
+layerBorderWidth = 1px
+layerBorderRadius = 5px
+
+.app-layer
+.layer-one
+    z-index: 1000
+    
+    &.background
+        background-color: layerOneSecondaryColor
+    &.border
+        border-width: layerBorderWidth
+        border-style: solid
+        border-color: layerOneBorderColor
+        &.round
+            border-radius: layerBorderRadius        
+
+.layer-two
+    z-index: 2000
+
+    &.background
+        background-color: layerTwoSecondaryColor
+    &.border
+        border-width: layerBorderWidth
+        border-style: solid
+        border-color: layerTwoBorderColor
+        &.round
+            border-radius: layerBorderRadius
+
+.layer-three
+    z-index: 3000
+
+    &.background
+        background-color: layerThreeSecondaryColor
+    &.border
+        border-width: layerBorderWidth
+        border-style: solid
+        border-color: layerThreeBorderColor
+        &.round
+            border-radius: layerBorderRadius
+
+.layer-four
+    z-index: 4000
+
+    &.background
+        background-color: layerFourSecondaryColor
+    &.border
+        border-width: layerBorderWidth
+        border-style: solid
+        border-color: layerFourBorderColor
+        &.round
+            border-radius: layerBorderRadius
 </style>
