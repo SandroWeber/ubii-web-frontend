0c768e893906735fb4a2daead0cb06ab0bf14b74
daniel <dyrda@in.tum.de>
2019-03-18 12:28:55 +0100

    Removed comment

--- src/components/ServerStatus.vue
+++ src/components/ServerStatus.vue
@@ -35,10 +35,7 @@
 <script>
   import UbiiClientService from '../services/ubiiClient/ubiiClientService.js';
   import { AppButton, AppLayer, AppInput } from './appComponents/appComponents.js';
-  //import AppButton from './appComponents/AppButton.vue';
-  //  import AppLayer from './appComponents/AppLayer.vue';
-   //   import AppInput from './appComponents/AppInput.vue';
-
+  
   /* fontawesome */
   import { library } from '@fortawesome/fontawesome-svg-core'
   import { faSyncAlt } from '@fortawesome/free-solid-svg-icons'
