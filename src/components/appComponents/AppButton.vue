1aa3e0b068f8c9e02370eac78e20b6586079ecf1
daniel <dyrda@in.tum.de>
2019-03-18 10:59:05 +0100

    Updated AppButton component

--- src/components/appComponents/AppButton.vue
+++ src/components/appComponents/AppButton.vue
@@ -1,16 +1,72 @@
 <template>
-  <div class="app-button">
-    <slot> </slot>
-  </div>
+  <button 
+    class="app-button"
+    @click="onClick"
+  >
+    <slot>
+    </slot>
+  </button>
 </template>
 
 <script>
   export default { 
     name: 'AppButton',
     components: {},
-    props: {}
+    props: {},
+    methods: {
+      onClick: function(value) {
+          this.$emit('click', value);
+      }
+    }
   } 
 </script> 
 
-<style scoped lang="stylus"> 
+<style scoped lang="stylus">
+@import "./../../styles/main/base/color"
+
+.app-button
+    border: 1px solid mediumContrastColor
+    color: mediumContrastColor
+    background-color: layerOneSecondaryColor
+
+    &.round
+      border-radius: 999px
+
+    // Contrast
+    &.low-contrast
+        color: lowContrastColor
+        border-color: lowContrastColor
+    &.medium-contrast
+        color: mediumContrastColor
+        border-color: mediumContrastColor
+    &.high-contrast
+        color: highContrastColor
+        border-color: highContrastColor
+    &.max-contrast
+        color: maxContrastColor
+        border-color: maxContrastColor
+
+    // Accents:
+    &.red-accent
+        color: redAccentColor
+        border-color: redAccentColor
+    &.green-accent
+        color: greenAccentColor
+        border-color: greenAccentColor
+    &.yellow-accent
+        color: yellowAccentColor
+        border-color: yellowAccentColor
+    &.orange-accent
+        color: orangeAccentColor
+        border-color: orangeAccentColor
+    &.purple-accent
+        color: purpleAccentColor
+        border-color: purpleAccentColor
+    &.blue-accent
+        color: blueAccentColor
+        border-color: blueAccentColor
+
+    &:hover
+        border-color: highContrastColor 
+        color: highContrastColor
 </style>
