880977d00b4c0fd45551dadf21f0dd830114aef9
daniel <dyrda@in.tum.de>
2019-03-18 11:52:58 +0100

    Implemnted AppInput component

---
+++ src/components/appComponents/AppInput.vue
@@ -0,0 +1,41 @@
+<template>
+    <input
+        class="app-input"
+        :id="id" 
+        :type="type"
+        v-model="localValue"
+    />
+</template>
+
+<script>
+
+    export default {
+        name: 'AppInput',
+        props: ['value', 'id', 'type'],
+        components: {
+        },
+        data: () => {
+            return {
+            };
+        },
+        computed: {
+            localValue: {
+                get() {
+                    return this.value
+                },
+                set(localValue) {
+                    this.$emit('input', localValue)
+                }
+            }
+        },
+        methods: {
+        }
+    }
+</script>
+
+<style scoped lang="stylus">
+    .code-mirror
+        height 100%
+        padding: 0px
+        flex-grow: 1
+</style>
