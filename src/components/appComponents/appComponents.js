cd4597b3f8a2855d351a9dacae4f18f09bf391ab
daniel <dyrda@in.tum.de>
2019-03-20 08:56:16 +0100

    Removed unused appIcon

--- src/components/appComponents/appComponents.js
+++ src/components/appComponents/appComponents.js
@@ -1,13 +1,11 @@
 import _AppLayer from './AppLayer.vue';
 import _AppButton from './AppButton.vue';
-import _AppIcon from './AppIcon.vue';
 import _AppInput from './AppInput.vue';
 import _AppTile from './AppTile.vue';
 import _AppToken from './AppToken.vue';
 
 export var AppLayer = _AppLayer;
 export var AppButton = _AppButton;
-export var AppIcon = _AppIcon;
 export var AppInput = _AppInput;
 export var AppTile = _AppTile;
 export var AppToken = _AppToken;
