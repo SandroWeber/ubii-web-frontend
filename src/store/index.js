23c6ad532e822bb202b57125124e514fe98a8b46
Daniel <dyrda@in.tum.de>
2019-04-02 19:20:57 +0200

    Added local storage utility to the store

--- src/store/index.js
+++ src/store/index.js
@@ -5,9 +5,25 @@ import sessions from './modules/sessions'
 
 Vue.use(Vuex)
 
-export default new Vuex.Store({
+let store = new Vuex.Store({
   modules: {
     interactions,
     sessions
   },
-})
+  mutations: {
+		initialiseStore(state) {
+			if(localStorage.getItem('store')) {
+				// If there is a store in the local storage, set the state object to the stored store.
+				this.replaceState(Object.assign(state, JSON.parse(localStorage.getItem('store'))));
+			}
+		}
+	},
+});
+
+// Subscribe to store updates.
+store.subscribe((mutation, state) => {
+	// Store the state object as a JSON string in the local storage.
+	localStorage.setItem('store', JSON.stringify(state));
+});
+
+export default store;
