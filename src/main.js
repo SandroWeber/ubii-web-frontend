067ff66796f3e0b698deeb19f7bea9eccc5e3dab
Daniel <dyrda@in.tum.de>
2019-04-02 19:22:54 +0200

    Added data persistence utility

--- src/main.js
+++ src/main.js
@@ -13,5 +13,8 @@ Vue.config.productionTip = false;
 new Vue({
     store,
     router,
-    render: h => h(App)
+    render: h => h(App),
+    beforeCreate() {
+      this.$store.commit('initialiseStore');
+    }
   }).$mount('#app');
