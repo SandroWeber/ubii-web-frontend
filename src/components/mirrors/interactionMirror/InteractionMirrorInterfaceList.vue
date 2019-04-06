04f0675a430ae75f00f4a2a424222f0e9ecd741f
daniel <dyrda@in.tum.de>
2019-04-06 14:14:50 +0200

    Added edit button

--- src/components/mirrors/interactionMirror/InteractionMirrorInterfaceList.vue
+++ src/components/mirrors/interactionMirror/InteractionMirrorInterfaceList.vue
@@ -5,24 +5,52 @@
         :key="element.internalName"
         :name="element.internalName"
         :interface-key="interfaceKey"
+        :editMode="editMode"
         :code="code"
       />
+
+      <app-button 
+        :class="'edit-button round low-contrast'"
+        @click="toggleEditMode"
+      >
+        <font-awesome-icon 
+        icon="plus"
+        class="tool-icon"
+        />
+    </app-button>
   </div>
 </template>
 
 <script>
   import InteractionMirrorInterfaceListToken from "./InteractionMirrorInterfaceListToken.vue";
 
+  import { library } from '@fortawesome/fontawesome-svg-core'
+  import { faPlus } from '@fortawesome/free-solid-svg-icons'
+  library.add(faPlus);
+
+  import { AppButton} from './../../appComponents/appComponents.js';
+
   export default { 
     name: 'interactionInterfaceList',
     components: {
-      InteractionMirrorInterfaceListToken: InteractionMirrorInterfaceListToken
+      InteractionMirrorInterfaceListToken: InteractionMirrorInterfaceListToken,
+      AppButton: AppButton,
     },
     props: {
       interfaceList: Array,
       interfaceKey: String,
       code: String
     },
+    data: () => {
+      return {
+        editMode: false
+      }
+    },
+    methods: {
+      toggleEditMode: function (){
+        this.editMode= this.editMode !== true;
+      }
+    }
     
   } 
 </script> 
@@ -35,4 +63,9 @@
     justify-content: flex-start
     align-items: flex-start
     align-content: flex-start
+
+  .edit-button
+    height: 1.5em
+    width: 1.5em
+    order: 1
 </style>
