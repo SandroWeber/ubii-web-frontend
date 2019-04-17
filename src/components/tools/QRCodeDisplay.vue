df6c4c0336ecfecaa3f16ea4276254af42546df9
Sandro Weber <webers@in.tum.de>
2019-04-17 16:17:10 +0200

    hotfix code styling

--- src/components/tools/QRCodeDisplay.vue
+++ src/components/tools/QRCodeDisplay.vue
@@ -1,33 +1,32 @@
 <template>
   <div>
     <p>Web Frontend over Wifi</p>
-    <br/>
+    <br>
     <canvas id="qrcode-canvas-wlan"></canvas>
   </div>
 </template>
 
 <script>
-  import QRCode from "qrcode";
-  import UbiiClientService from '../../services/ubiiClient/ubiiClientService.js';
+import QRCode from "qrcode";
+import UbiiClientService from "../../services/ubiiClient/ubiiClientService.js";
 
 /* eslint-disable no-console */
 
 export default {
   name: "QRCodeDisplay",
-  data: () => {
+  data() {
     return {};
   },
   mounted() {
-    let canvas = document.getElementById('qrcode-canvas-wlan');
-    let url = UbiiClientService.client.serverSpecification.ipWlan + ':' + location.port
+    let canvas = document.getElementById("qrcode-canvas-wlan");
+    let url =
+      UbiiClientService.client.serverSpecification.ipWlan + ":" + location.port;
 
-    QRCode.toCanvas(canvas, url, {width: 500},
-      (error) => {
+    QRCode.toCanvas(canvas, url, { width: 500 }, error => {
       if (error) console.error(error);
     });
   }
 };
 </script>
 
-<style scoped lang="stylus">
-</style>
+<style scoped lang="stylus"></style>
