50fb7d3275ebafdb7b250c218e0f0ee5702d1171
Daniel <dyrda@in.tum.de>
2019-05-02 19:52:25 +0200

    Updated indentation

--- src/components/appComponents/AppInput.vue
+++ src/components/appComponents/AppInput.vue
@@ -1,56 +1,56 @@
 <template>
-    <input
-        class="app-input"
-        :class="size"
-        :id="id" 
-        :type="type"
-        v-model="localValue"
-    />
+  <input
+    class="app-input"
+    :class="size"
+    :id="id" 
+    :type="type"
+    v-model="localValue"
+  />
 </template>
 
 <script>
-    export default {
-        name: 'AppInput',
-        props: [
-            'value',
-            'id',
-            'type',
-            'size'
-        ],
-        computed: {
-            localValue: {
-                get() {
-                    return this.value
-                },
-                set(localValue) {
-                    this.$emit('input', localValue)
-                }
-            }
+  export default {
+    name: 'AppInput',
+    props: [
+      'value',
+      'id',
+      'type',
+      'size'
+    ],
+    computed: {
+      localValue: {
+        get() {
+          return this.value
         },
-    }
+        set(localValue) {
+          this.$emit('input', localValue)
+        }
+      }
+    },
+  }
 </script>
 
 <style scoped lang="stylus">
 @import "./../../styles/main/color"
 
 .app-input
-    z-index: 4000
-    padding-left 3px
-    color: highContrastColor
+  z-index: 4000
+  padding-left 3px
+  color: highContrastColor
+  background-color: layerFourSecondaryColor
+  border-width: 1px
+  border-style: solid
+  border-color: layerFourBorderColor
+  &.round
+    border-radius: layerBorderRadius
+  &.huge
+    font-size 1.3em
+  &.layer-one
+    background-color: layerOneSecondaryColor
+  &.layer-two
+    background-color: layerTwoSecondaryColor
+  &.layer-three
+    background-color: layerThreeSecondaryColor
+  &.layer-four
     background-color: layerFourSecondaryColor
-    border-width: 1px
-    border-style: solid
-    border-color: layerFourBorderColor
-    &.round
-        border-radius: layerBorderRadius
-    &.huge
-        font-size 1.3em
-    &.layer-one
-        background-color: layerOneSecondaryColor
-    &.layer-two
-        background-color: layerTwoSecondaryColor
-    &.layer-three
-        background-color: layerThreeSecondaryColor
-    &.layer-four
-        background-color: layerFourSecondaryColor
 </style>
--- src/components/appComponents/AppToken.vue
+++ src/components/appComponents/AppToken.vue
@@ -15,15 +15,15 @@
     name: 'AppToken',
     components: {},
     props: {
-        text: String,
-        useFixedCornerRadius: Boolean
+      text: String,
+      useFixedCornerRadius: Boolean
     },
     computed:{
-        computedClassList: function(){
-            return {
-                "fixed-corner-radius": this.useFixedCornerRadius
-            }
+      computedClassList: function(){
+        return {
+          "fixed-corner-radius": this.useFixedCornerRadius
         }
+      }
     }
   } 
 </script> 
@@ -32,63 +32,63 @@
 @import "./../../styles/main/color"
 
 .app-token
-    // Base:
-    background-color: mediumContrastColor
-    border-radius: 999px
+  // Base:
+  background-color: mediumContrastColor
+  border-radius: 999px
+  color: layerOneSecondaryColor
+  padding 5px 10px 5px 10px
+
+  &.fixed-corner-radius
+    border-radius: 1em
+
+  // Primary:
+  &.layer-one
     color: layerOneSecondaryColor
-    padding 5px 10px 5px 10px
+  &.layer-two
+    color: layerTwoSecondaryColor
+  &.layer-three
+    color: layerThreeSecondaryColor
+  &.layer-four
+    color: layerFourSecondaryColor
 
-    &.fixed-corner-radius
-        border-radius: 1em
-
-    // Primary:
-    &.layer-one
-        color: layerOneSecondaryColor
-    &.layer-two
-        color: layerTwoSecondaryColor
-    &.layer-three
-        color: layerThreeSecondaryColor
-    &.layer-four
-        color: layerFourSecondaryColor
-
-    // Contrasts
-    &.low-contrast
-        background-color: lowContrastColor
-    &.medium-contrast
-        background-color: mediumContrastColor
-    &.high-contrast
-        background-color: highContrastColor
-    &.max-contrast
-        background-color: maxContrastColor
+  // Contrasts
+  &.low-contrast
+    background-color: lowContrastColor
+  &.medium-contrast
+    background-color: mediumContrastColor
+  &.high-contrast
+    background-color: highContrastColor
+  &.max-contrast
+    background-color: maxContrastColor
 
 
-    // Accents:
-    &.red-accent
-        background-color: redAccentColor
-    &.green-accent
-        background-color: greenAccentColor
-    &.yellow-accent
-        background-color: yellowAccentColor
-    &.orange-accent
-        background-color: orangeAccentColor
-    &.purple-accent
-        background-color: purpleAccentColor
-    &.blue-accent
-        background-color: blueAccentColor
+  // Accents:
+  &.red-accent
+    background-color: redAccentColor
+  &.green-accent
+    background-color: greenAccentColor
+  &.yellow-accent
+    background-color: yellowAccentColor
+  &.orange-accent
+    background-color: orangeAccentColor
+  &.purple-accent
+    background-color: purpleAccentColor
+  &.blue-accent
+    background-color: blueAccentColor
 
 .token-text
-    color: maxContrastColor 
+  color: maxContrastColor 
 
 .layer-one
-    & .token-text
-        color: layerOneSecondaryColor
+  & .token-text
+    color: layerOneSecondaryColor
 .layer-two
-    & .token-text  
-        color: layerTwoSecondaryColor
+  & .token-text  
+    color: layerTwoSecondaryColor
 .layer-three
-    & .token-text
-        color: layerThreeSecondaryColor
+  & .token-text
+    color: layerThreeSecondaryColor
 .layer-four
-    & .token-text
-        color: layerFourSecondaryColor
+  & .token-text
+    color: layerFourSecondaryColor
 </style>
