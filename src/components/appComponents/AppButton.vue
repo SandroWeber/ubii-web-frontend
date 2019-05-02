32be47654453d3f7adc19b2a844dfdbb6e00d4dd
Daniel <dyrda@in.tum.de>
2019-05-02 19:49:33 +0200

    Updated indentation

--- src/components/appComponents/AppButton.vue
+++ src/components/appComponents/AppButton.vue
@@ -40,52 +40,52 @@
 @import "./../../styles/main/color"
 
 .app-button
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    border: 1px solid mediumContrastColor
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  border: 1px solid mediumContrastColor
+  color: mediumContrastColor
+  background-color: layerOneSecondaryColor
+  cursor pointer
+
+  &.round
+    border-radius: 999px
+
+  // Contrast
+  &.low-contrast
+    color: lowContrastColor
+    border-color: lowContrastColor
+  &.medium-contrast
     color: mediumContrastColor
-    background-color: layerOneSecondaryColor
-    cursor pointer
+    border-color: mediumContrastColor
+  &.high-contrast
+    color: highContrastColor
+    border-color: highContrastColor
+  &.max-contrast
+    color: maxContrastColor
+    border-color: maxContrastColor
 
-    &.round
-      border-radius: 999px
+  // Accents:
+  &.red-accent
+    color: redAccentColor
+    border-color: redAccentColor
+  &.green-accent
+    color: greenAccentColor
+    border-color: greenAccentColor
+  &.yellow-accent
+    color: yellowAccentColor
+    border-color: yellowAccentColor
+  &.orange-accent
+    color: orangeAccentColor
+    border-color: orangeAccentColor
+  &.purple-accent
+    color: purpleAccentColor
+    border-color: purpleAccentColor
+  &.blue-accent
+    color: blueAccentColor
+    border-color: blueAccentColor
 
-    // Contrast
-    &.low-contrast
-        color: lowContrastColor
-        border-color: lowContrastColor
-    &.medium-contrast
-        color: mediumContrastColor
-        border-color: mediumContrastColor
-    &.high-contrast
-        color: highContrastColor
-        border-color: highContrastColor
-    &.max-contrast
-        color: maxContrastColor
-        border-color: maxContrastColor
-
-    // Accents:
-    &.red-accent
-        color: redAccentColor
-        border-color: redAccentColor
-    &.green-accent
-        color: greenAccentColor
-        border-color: greenAccentColor
-    &.yellow-accent
-        color: yellowAccentColor
-        border-color: yellowAccentColor
-    &.orange-accent
-        color: orangeAccentColor
-        border-color: orangeAccentColor
-    &.purple-accent
-        color: purpleAccentColor
-        border-color: purpleAccentColor
-    &.blue-accent
-        color: blueAccentColor
-        border-color: blueAccentColor
-
-    &:hover
-        border-color: highContrastColor 
-        color: highContrastColor
+  &:hover
+    border-color: highContrastColor 
+    color: highContrastColor
 </style>
