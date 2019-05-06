d0efdd62ec88cbe4d3f7bab3e5be53edf0b3fe8d
daniel <dyrda@in.tum.de>
2019-05-06 10:51:45 +0200

    Added AppExplorer component

---
+++ src/components/appComponents/AppExplorer.vue
@@ -0,0 +1,25 @@
+<template>
+
+</template>
+
+<script>
+  export default {
+    name: 'AppExplorer',
+    props: [
+    ],
+    computed: {
+      localValue: {
+        get() {
+          return this.value
+        },
+        set(localValue) {
+          this.$emit('input', localValue)
+        }
+      }
+    },
+  }
+</script>
+
+<style scoped lang="stylus">
+@import "./../../styles/main/color"
+</style>
