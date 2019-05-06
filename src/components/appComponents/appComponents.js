bc56631524e4fd4fd2b76c2c387d0ece653ac106
daniel <dyrda@in.tum.de>
2019-05-06 11:13:03 +0200

    Added AppExplorer to the ppComponents export

--- src/components/appComponents/appComponents.js
+++ src/components/appComponents/appComponents.js
@@ -3,9 +3,11 @@ import _AppButton from './AppButton.vue';
 import _AppInput from './AppInput.vue';
 import _AppTile from './AppTile.vue';
 import _AppToken from './AppToken.vue';
+import _AppExplorer from './AppExplorer.vue';
 
 export var AppLayer = _AppLayer;
 export var AppButton = _AppButton;
 export var AppInput = _AppInput;
 export var AppTile = _AppTile;
 export var AppToken = _AppToken;
+export var AppExplorer = _AppExplorer;
