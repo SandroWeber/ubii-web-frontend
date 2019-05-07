bdb09e2a8df57a256dc13022ec0f3eb952214780
daniel <dyrda@in.tum.de>
2019-05-07 18:32:15 +0200

    Added appExplorer components

---
+++ src/components/appComponents/appExplorer/AppExplorerItem.vue
@@ -0,0 +1,65 @@
+<template>
+  <div
+    class="interaction-explorer-item low-contrast"
+    @click="selectItem(id)"
+    v-bind:class="{ selected: selected }"
+  >
+    <font-awesome-icon 
+      icon="code"
+      class="icon"
+    />
+    <span 
+      class="label"
+    >
+      {{name}}
+    </span>
+  </div>
+</template>
+
+<script>
+  import { library } from '@fortawesome/fontawesome-svg-core'
+  import { faCode } from '@fortawesome/free-solid-svg-icons'
+  library.add(faCode);
+
+  export default { 
+    name: 'InteractionExplorerItem',
+    components: {},
+    props: {
+      id: String,
+      name: String,
+      selected: Boolean
+    },
+    methods: {
+      selectItem: function(id) {
+          this.$emit('select', id);
+      }
+    }
+  } 
+</script> 
+
+<style scoped lang="stylus"> 
+  .interaction-explorer-item
+    order 1
+    padding 5px 10px 5px 10px
+    display flex
+    flex-direction row
+    cursor pointer
+    overflow: hidden
+    white-space: nowrap;
+    &:hover
+      color #ffffff
+    &.selected
+      background-color layerThreeSecondaryColor
+      color yellowAccentColor
+
+  .icon
+    height 1em
+    width 1em
+    margin-top: 0px
+    
+  .label
+    margin-left 5px
+    font-size 1em
+    margin-top 0.05em
+
+</style>
