3c266f607b0818cf558dd196f9c2d3b5a7801ed5
daniel <dyrda@in.tum.de>
2019-05-07 18:29:17 +0200

    Moved appexplorer to own folder

---
+++ src/components/appComponents/appExplorer/AppExplorer.vue
@@ -0,0 +1,46 @@
+<template>
+    <app-layer class="app-explorer layer-two background shadow">
+    <app-explorer-toolbar
+      :selectedRecords="selectedRecords"
+      @delete="deleteRecords()"
+    />
+    <app-explorer-item
+      v-for="record in records"
+      :key="record.id"
+      :label="record.label"
+      :id="record.id"
+      :selected="record.selected"
+      @select="selectRecord(record)"
+      @deselect="deselectRecord(record)"
+    />
+  </app-layer>
+</template>
+
+<script>
+  export default {
+    name: 'AppExplorer',
+    props: {
+      records: Array,
+    },
+    computed: {
+      selectedRecords: function(){
+        // TODO return all records that are flagged with selected or the first 
+      },
+    },
+    methods: {
+      deleteRecords: function(){
+        // TODO delete currently selected records
+      },
+      selectRecord: function(record){
+        record.selected = true;
+      },
+      deselectRecord: function(record){
+        record.selected = false;
+      },
+    }
+  }
+</script>
+
+<style scoped lang="stylus">
+@import "./../../styles/main/color"
+</style>
