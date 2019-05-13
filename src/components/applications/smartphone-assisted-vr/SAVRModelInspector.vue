d5acedd4d57e7f4ddccd0fa44b3b6e8614f3a0bb
Michael Lohr <michael.lohr@tum.de>
2019-05-13 13:52:25 +0200

    Added UbiInteract Networking code back in

--- src/components/applications/smartphone-assisted-vr/SAVRModelInspector.vue
+++ src/components/applications/smartphone-assisted-vr/SAVRModelInspector.vue
@@ -1,18 +1,39 @@
 <template>
-  <div>
-    <div id="savr-model-inspector-render-container" class="render-container"></div>
+  <div class="full-height">
+    <div v-show="!ubiiClientService.isConnected">
+        <span class="notification">Please connect to backend before starting the application.</span>
+    </div>
+
+    <div v-show="ubiiClientService.isConnected" class="full-height">
+      <div id="savr-model-inspector-render-container" class="full-height"></div>
+    </div>
   </div>
 </template>
 
 <script>
   /* eslint-disable no-console */
 
+  // Rendering
   import * as THREE from 'three'
   import WEBVR from '../sharedModules/moduleThreejsWebVR'
   import SKY from '../sharedModules/moduleThreejsSky'
 
+  // Networking
+  import {DEFAULT_TOPICS} from '@tum-far/ubii-msg-formats';
+  import UbiiClientService from '../../../services/ubiiClient/ubiiClientService.js';
+
+  let ENABLE_VR = false;
+
   export default {
     name: 'SAVRModelInspector',
+    mounted() {
+      this.init();
+      this.animate();
+      this.subscribe();
+    },
+    beforeDestroy: function () {
+      this.stopDemo();
+    },
     data() {
       return {
         camera: null,
@@ -21,6 +42,9 @@
         mesh: null,
         time: 0,
         clock: new THREE.Clock(),
+        ubiiClientService: UbiiClientService,
+        clients: new Map(),
+        pollSmartDevices: false
       }
     },
     methods: {
@@ -52,36 +76,96 @@
         container.appendChild(this.renderer.domElement);
         this.renderer.autoClear = false;
 
-        container.appendChild(WEBVR.createButton(this.renderer));
-        this.renderer.vr.enabled = true;
+        if (ENABLE_VR)
+        {
+          container.appendChild( WEBVR.createButton( this.renderer ) );
+          this.renderer.vr.enabled = true;
+        }
+
       },
       animate: function() {
-
         let renderer = this.renderer;
         let scene = this.scene;
         let camera = this.camera;
         let mesh = this.mesh;
+        // eslint-disable-next-line
         let time = this.time;
         let clock = this.clock;
+        let clients = this.clients;
 
         this.renderer.setAnimationLoop( function () {
           let delta = clock.getDelta();
-
-          mesh.rotation.x += 0.01;
-          mesh.rotation.y += 0.02;
-          
           time += delta;
+
+          clients.forEach((client) => {
+            if (typeof client.orientation.x !== 'undefined') {
+              mesh.rotation.x = THREE.Math.degToRad(client.orientation.y);
+              mesh.rotation.y = THREE.Math.degToRad(client.orientation.x);
+              mesh.rotation.z = THREE.Math.degToRad(-client.orientation.z);
+            }
+          });
 
           renderer.clear();
           renderer.render(scene, camera);
-
         });
       },
+      subscribe: function() {
+        window.addEventListener('beforeunload', () => { // unsubscribe before page is unloaded
+          this.stopDemo();
+        });
+
+        this.startDemo();
+      },
+
+      startDemo: function () {
+        this.$data.pollSmartDevices = true;
+        this.updateSmartDevices();
+      },
+
+      stopDemo: function () {
+        this.$data.pollSmartDevices = false;
+      },
+
+      updateSmartDevices: function () {
+        UbiiClientService.client.callService({topic: DEFAULT_TOPICS.SERVICES.TOPIC_LIST})
+          .then((reply) => {
+            this.$data.topicList = reply.stringList.list;
+
+            this.$data.topicList.forEach((topic) => {
+              let smart_device_topic_index = topic.indexOf('/web-interface-smart-device/orientation');
+
+              if (smart_device_topic_index !== -1) {
+                let clientID = topic.substring(0, smart_device_topic_index);
+
+                if (!this.$data.clients.has(clientID)) {
+                  this.addClient(clientID, topic);
+                }
+              }
+            })
+          });
+
+        if (this.$data.pollSmartDevices) {
+          setTimeout(this.updateSmartDevices, 1000);
+        }
+      },
+
+      addClient: function(clientID, topic) {
+        // create client object with necessary info
+        let client = {
+          topicOrientation: topic,
+          orientation: {x: undefined, y: undefined, z: undefined}
+        };
+
+        this.$data.clients.set(clientID, client);
+
+        UbiiClientService.client.subscribe(
+          client.topicOrientation,
+          (orientation) => {
+            client.orientation = orientation;
+          }
+        );
+      }
     },
-    mounted() {
-      this.init();
-      this.animate();
-    }
   }
 </script>
 
