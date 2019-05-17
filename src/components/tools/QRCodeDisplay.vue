7fb2229ed346411b4eee56551f6ac2a241165cd2
Sandro Weber <webers@in.tum.de>
2019-05-17 11:02:44 +0200

    extended QRCode display with ethernet

--- src/components/tools/QRCodeDisplay.vue
+++ src/components/tools/QRCodeDisplay.vue
@@ -1,10 +1,20 @@
 <template>
-  <div>
-    <p>Web Frontend over Wifi</p>
-    <p class="warning" v-show="urlWlan === undefined">No Wifi Address!</p>
-    <p v-show="urlWlan !== undefined">{{urlWlan}}</p>
-    <br>
-    <canvas id="qrcode-canvas-wlan"></canvas>
+  <div class="grid">
+    <div>
+      <p>Wifi Connection</p>
+      <p class="warning" v-show="urlWlan === undefined">No Wifi Address!</p>
+      <p v-show="urlWlan !== undefined">{{urlWlan}}</p>
+      <br>
+      <canvas id="qrcode-canvas-wlan"></canvas>
+    </div>
+
+    <div>
+      <p>Ethernet Connection</p>
+      <p class="warning" v-show="urlEthernet === undefined">No Ethernet Address!</p>
+      <p v-show="urlEthernet !== undefined">{{urlEthernet}}</p>
+      <br>
+      <canvas id="qrcode-canvas-ethernet"></canvas>
+    </div>
   </div>
 </template>
 
@@ -19,18 +29,33 @@ export default {
   data() {
     return {
       ubiiClientService: UbiiClientService,
-      urlWlan: undefined
+      urlWlan: undefined,
+      urlEthernet: undefined
     };
   },
   mounted() {
     if (UbiiClientService.client.serverSpecification.ipWlan.length > 0) {
       let canvas = document.getElementById("qrcode-canvas-wlan");
-      
-      this.urlWlan = UbiiClientService.client.serverSpecification.ipWlan +
+
+      this.urlWlan =
+        UbiiClientService.client.serverSpecification.ipWlan +
         ":" +
         location.port;
 
       QRCode.toCanvas(canvas, this.urlWlan, { width: 500 }, error => {
+        if (error) console.error(error);
+      });
+    }
+
+    if (UbiiClientService.client.serverSpecification.ipEthernet.length > 0) {
+      let canvas = document.getElementById("qrcode-canvas-ethernet");
+
+      this.urlEthernet =
+        UbiiClientService.client.serverSpecification.ipEthernet +
+        ":" +
+        location.port;
+
+      QRCode.toCanvas(canvas, this.urlEthernet, { width: 500 }, error => {
         if (error) console.error(error);
       });
     }
@@ -39,6 +64,13 @@ export default {
 </script>
 
 <style scoped lang="stylus">
-  .warning
-    color: red
+.warning {
+  color: red;
+}
+
+.grid
+    display: grid
+    grid-gap: 25px
+    grid-template-columns: 1fr 1fr
+    margin: 25px
 </style>
