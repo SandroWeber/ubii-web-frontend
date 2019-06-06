4509f5668cec051d7a8098821d018440dbf3d41a
Michael Lohr <michael.lohr@tum.de>
2019-06-06 16:56:23 +0200

    WIP: Added cursor

--- src/components/applications/smartphoneAssistedVR/modules/SmartphoneCursor.js
+++ src/components/applications/smartphoneAssistedVR/modules/SmartphoneCursor.js
@@ -1,11 +1,21 @@
 import * as THREE from "three";
 
+
 export default class SmartphoneCursor extends THREE.Object3D {
 
   constructor() {
     super();
 
     this.name = "Smartphone Cursor";
+
+    const geom = new THREE.PlaneBufferGeometry();
+    //geom.scale.set(0.8, 0.8, 0.8)
+    this.add(new THREE.Mesh(geom, new THREE.MeshBasicMaterial({
+      transparent: true,
+      map: new THREE.TextureLoader().load("images/circle.png")
+    })))
+
+
   }
 
 }
