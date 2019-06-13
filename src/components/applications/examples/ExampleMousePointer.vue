e36e4a02b3b40fcc0cca30973b7c2eabb314705e
Sandro Weber <webers@in.tum.de>
2019-06-13 15:17:22 +0200

    [hotfix] mouse demo broken on start

--- src/components/applications/examples/ExampleMousePointer.vue
+++ src/components/applications/examples/ExampleMousePointer.vue
@@ -70,8 +70,13 @@ export default {
       this.stopExample();
     });
 
-    UbiiEventBus.$on(UbiiEventBus.CONNECT_EVENT, this.startExample);
+    UbiiEventBus.$on(UbiiEventBus.CONNECT_EVENT, () => {
+      this.stopExample();
+      this.startExample();
+    });
     UbiiEventBus.$on(UbiiEventBus.DISCONNECT_EVENT, this.stopExample);
+    
+    this.startExample();
   },
   beforeDestroy: function() {
     this.stopExample();
