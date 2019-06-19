9c42fcf758a7587459c4a42ca5eda1415eced52a
Sandro Weber <webers@in.tum.de>
2019-06-19 11:39:35 +0200

    basic config, FF CORS issue = untrusted certificate

--- vue.config.js
+++ vue.config.js
@@ -5,6 +5,13 @@ module.exports = {
     const types = ['vue-modules', 'vue', 'normal-modules', 'normal']
     types.forEach(type => addStyleResource(config.module.rule('stylus').oneOf(type)))
   },
+  devServer: {
+    //open: process.platform === 'darwin',
+    host: '127.0.0.1',
+    //port: 8080, // CHANGE YOUR PORT HERE!
+    https: true,
+    //hotOnly: false,
+  },
 }
 
 function addStyleResource (rule) {
