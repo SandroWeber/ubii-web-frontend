08c588663424c9f3257db512f76e2447c374192c
Sandro Weber <webers@in.tum.de>
2019-07-29 22:28:58 +0200

    camera interface catch rejected cam access

--- src/components/interfaces/Camera.vue
+++ src/components/interfaces/Camera.vue
@@ -31,14 +31,20 @@ export default {
     // Get access to the camera!
     if (navigator.mediaDevices && navigator.mediaDevices.getUserMedia) {
       // Not adding `{ audio: true }` since we only want video now
-      navigator.mediaDevices.getUserMedia({ video: true }).then(stream => {
-        //video.src = window.URL.createObjectURL(stream);
-        video.srcObject = stream;
-        video.play();
+      navigator.mediaDevices.getUserMedia({ video: true }).then(
+        //resolved
+        stream => {
+          //video.src = window.URL.createObjectURL(stream);
+          video.srcObject = stream;
+          video.play();
 
-        this.videoElement = video;
-        this.start();
-      });
+          this.videoElement = video;
+          this.start();
+        },
+        //rejected
+        (error) => {
+          console.warn(error);
+        });
     }
   },
   beforeDestroy: function() {
