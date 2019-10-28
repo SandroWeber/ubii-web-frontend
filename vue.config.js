37dde24babf1d9a5e28ae77ecdb8cf67127b371e
Sandro Weber <webers@in.tum.de>
2019-10-28 18:36:19 +0100

    remove devServer, add nginx.conf, rename certificates folder

--- vue.config.js
+++ vue.config.js
@@ -1,11 +1,13 @@
 const path = require('path');
-const fs = require('fs');
+//const fs = require('fs');
 
 module.exports = {
   chainWebpack: config => {
-    const types = ['vue-modules', 'vue', 'normal-modules', 'normal']
-    types.forEach(type => addStyleResource(config.module.rule('stylus').oneOf(type)))
-  },
+    const types = ['vue-modules', 'vue', 'normal-modules', 'normal'];
+    types.forEach(type =>
+      addStyleResource(config.module.rule('stylus').oneOf(type))
+    );
+  } /*,
   devServer: {
     //open: process.platform === 'darwin',
     host: 'localhost',
@@ -15,16 +17,17 @@ module.exports = {
       cert: fs.readFileSync('./certs/ubii.com+5.pem')
     }
     //hotOnly: false,
-  },
-}
+  },*/
+};
 
 function addStyleResource(rule) {
-  rule.use('style-resource')
+  rule
+    .use('style-resource')
     .loader('style-resources-loader')
     .options({
       patterns: [
         path.resolve(__dirname, './src/styles/main/main.styl'),
-        path.resolve(__dirname, './src/styles/rete.styl'),
-      ],
-    })
+        path.resolve(__dirname, './src/styles/rete.styl')
+      ]
+    });
 }
