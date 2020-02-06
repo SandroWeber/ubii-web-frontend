b2f6fbd3ff530b935b199172b1c75a997f4e9dfd
Sandro Weber <webers@in.tum.de>
2020-02-06 13:22:26 +0100

    tests running ok

--- src/components/tools/PerformanceTestFibonacci.vue
+++ src/components/tools/PerformanceTestFibonacci.vue
@@ -7,63 +7,54 @@
       @click="startTest()"
       :disabled="!ubiiClientService.isConnected"
     >
-      <font-awesome-icon
-        icon="play"
-        v-show="this.testData.status !== 'running'"
-      />
-      <font-awesome-icon
-        icon="spinner"
-        v-show="this.testData.status === 'running'"
-      />
+      <font-awesome-icon icon="play" v-show="this.testData.status !== 'running'" />
+      <font-awesome-icon icon="spinner" v-show="this.testData.status === 'running'" />
     </app-button>
 
     <div class="statistics-grid">
       <span>Status:</span>
       <span class="test-status">{{ this.testData.status }}</span>
-      <span>Number of correctly processed iterations:</span>
-      <span class="test-status">{{
+      <span>Number of processed iterations:</span>
+      <span class="test-status">
+        {{
         this.testData.numProcessingIterations
-      }}</span>
+        }}
+      </span>
     </div>
 
     <div class="separator"></div>
 
     <div class="settings-grid">
-      <label for="fibonacci-session-count" class="setting-label"
-        ># sessions:</label
-      >
+      <label for="fibonacci-session-count" class="setting-label"># sessions:</label>
       <app-input
         :id="'fibonacci-session-count'"
         :type="'# sessions'"
         v-model="testData.sessionCount"
       />
 
-      <label for="fibonacci-interaction-count" class="setting-label"
-        ># interactions:</label
-      >
+      <label for="fibonacci-interaction-count" class="setting-label"># interactions:</label>
       <app-input
         :id="'fibonacci-interaction-count'"
         :type="'# interactions'"
         v-model="testData.interactionCountPerSession"
       />
 
-      <label
-        for="fibonacci-sequence-length"
-        class="setting-label setting-3-label"
-        >fib sequence length (n):</label
-      >
+      <label for="fibonacci-sequence-length" class="setting-label">fib sequence length (n):</label>
       <app-input
         :id="'fibonacci-sequence-length'"
-        class="setting-3-input"
         :type="'# interactions'"
         v-model="testData.fibSequenceLength"
       />
+
+      <label for="test-duration" class="setting-label">test duration (seconds):</label>
+      <app-input :id="'test-duration'" :type="'test duration'" v-model="testData.testDuration" />
     </div>
   </div>
 </template>
 
 <script>
 import ProtobufLibrary from '@tum-far/ubii-msg-formats/dist/js/protobuf';
+import { DEFAULT_TOPICS } from '@tum-far/ubii-msg-formats';
 /* fontawesome */
 import { library } from '@fortawesome/fontawesome-svg-core';
 import { faPlay, faSpinner } from '@fortawesome/free-solid-svg-icons';
@@ -96,9 +87,9 @@ export default {
         sessionCount: '1',
         interactionCountPerSession: '5',
         fibSequenceLength: '20',
+        testDuration: '5',
         status: 'unmeasured',
         numProcessingIterations: 'N/A',
-        processingInteractionIDs: [],
         allSessionsSpecs: []
         /*device: {
           name: 'fibonacci-performance-test-device',
@@ -131,6 +122,9 @@ export default {
       }*/
     },
     prepareTest: function() {
+      this.testData.processingCountMap = new Map();
+      this.testData.processingFinished = [];
+
       // create all the specs for sessions and interactions
       this.testData.allSessionsSpecs = PerformanceTestFibonacciHelper.createTestSpecs(
         this.testData.sessionCount,
@@ -183,13 +177,62 @@ export default {
       /*await UbiiClientService.unsubscribe(
         this.testData.device.components[0].topic
       );*/
+      this.testData.numProcessingIterations = 'N/A';
 
       this.prepareTest();
+      console.info(this.testData.processingFinished);
+
+      this.testData.allSessionsSpecs.forEach(session => {
+        UbiiClientService.client.callService({
+          topic: DEFAULT_TOPICS.SERVICES.SESSION_START,
+          session: session
+        });
+      });
 
       this.testData.status = 'running';
+      this.testData.startTime = Date.now();
+      setTimeout(() => {
+        this.stopTest();
+      }, parseInt(this.testData.testDuration) * 1000);
     },
-    stopTest: async function() {},
-    onProcessingFinishedCallback: function(topic, float) {}
+    stopTest: async function() {
+      this.testData.status = 'done';
+      this.testData.stopTime = Date.now();
+      this.testData.allSessionsSpecs.forEach(session => {
+        UbiiClientService.client.callService({
+          topic: DEFAULT_TOPICS.SERVICES.SESSION_STOP,
+          session: session
+        });
+      });
+
+      this.testData.processingFinished.forEach(id => {
+        if (!this.testData.processingCountMap.has(id)) {
+          let count = this.testData.processingFinished.reduce(
+            (n, x) => n + (x === id),
+            0
+          );
+          this.testData.processingCountMap.set(id, count);
+        }
+      });
+
+      let passedTime = this.testData.stopTime - this.testData.startTime;
+      console.info('passed time (ms): ' + passedTime);
+      let numProcessingIterations = 0;
+      this.testData.processingCountMap.forEach(value => {
+        numProcessingIterations += value;
+      });
+      console.info('numProcessingIterations: ' + numProcessingIterations);
+      this.testData.numProcessingIterations = numProcessingIterations;
+    },
+    onProcessingFinishedCallback: function(float, topic) {
+      if (this.testData.status === 'running') {
+        /*let count = this.testData.processingCountMap.get(id);
+        this.testData.processingCountMap.set(id, count++);
+        console.info(id + ' ' + count);*/
+        let id = topic.substring(1, 37); // uuidv4 is 36 chars long
+        this.testData.processingFinished.push(id);
+      }
+    }
   }
 };
 </script>
